0b4a5d6c30e59e1e4fcebfea0cc6b67eab680b61
David Cowen <55639485+David-Cowen@users.noreply.github.com>
2023-06-26 07:13:10 +1000

    Add files via upload

---
+++ R/testPackageFunction.R
@@ -0,0 +1,16 @@
+#' This is the test function title
+#'
+#' This is the description of thwat the test function will do.
+#'
+#' @param x This is a decription of what parameter x would be if it were used in the function.
+#' @return This would be a description of the returned value (which should be 16).
+#' @examples
+#' testPackageFunction()
+#' @export
+
+testPackageFunction <- function(){
+  a <- 5
+  b <- 11
+  c <- a + b
+  return(c)
+}
